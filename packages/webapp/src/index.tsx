684d11b533a8ac97c686449e7a974ecc006c888b
Tyler Schloesser <tylerschloesser@gmail.com>
2021-11-13 09:22:18 -0800

    add test tailwind component

--- packages/webapp/src/index.tsx
+++ packages/webapp/src/index.tsx
@@ -36,7 +36,20 @@ const root = document.getElementById('root')!
 
 const App = () => {
   const { data } = useQuery(POSTS_QUERY)
-  return <h1>data: {JSON.stringify(data)}</h1>
+  return (
+    <>
+      <h1>data: {JSON.stringify(data)}</h1>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div className="flex-shrink-0">
+          <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-gray-500">You have a new message!</p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 ReactDom.render(
