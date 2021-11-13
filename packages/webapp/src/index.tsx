705a173b5b95a890f3595c6794bc4718ad747bf7
Tyler Schloesser <tylerschloesser@gmail.com>
2021-11-13 10:15:25 -0800

    update copy

--- packages/webapp/src/index.tsx
+++ packages/webapp/src/index.tsx
@@ -70,7 +70,7 @@ const PostsContainer = () => {
       <div className="mt-8 p-4 text-center rounded-md bg-gray-200">
         You've got ideas, set them free.{' '}
         <button className="ml-2 py-2 px-4 bg-black text-white text-sm rounded-full">
-          Write on Medium
+          Write something
         </button>
       </div>
     </div>
