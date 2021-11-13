4a012b64f04fb846b82d954768a8a578a91eda9a
Tyler Schloesser <tylerschloesser@gmail.com>
2021-11-13 09:51:23 -0800

    add flex-2

--- packages/webapp/tailwind.config.js
+++ packages/webapp/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      flex: {
+        '2': '2 2 0%',
+      }
+    },
   },
   variants: {
     extend: {},
