3704ad3cb15a1a981e363e01b71038ac9076f7f5
Tyler Schloesser <tylerschloesser@gmail.com>
2021-11-20 08:59:04 -0800

    fetch from postgres

--- packages/server/test.ts
+++ packages/server/test.ts
@@ -65,20 +65,6 @@ const db = {
 }
 
 // https://source.unsplash.com/random/200x150
-const TEST_POSTS = [
-  {
-    title: 'The quick brow fox',
-    author: 'Tyler Schloesser',
-    image:
-      'https://images.unsplash.com/photo-1634534904807-b676da34dc79?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=150&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzNjgyNTAyOA&ixlib=rb-1.2.1&q=80&w=200',
-  },
-  {
-    title: 'My Struggle',
-    author: 'Bill Clinton',
-    image:
-      'https://images.unsplash.com/photo-1636654286371-a2496896eb6d?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=150&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzNjgyNTA2Nw&ixlib=rb-1.2.1&q=80&w=200',
-  },
-].map((p, i) => ({ id: `test-${i}`, ...p }))
 
 const PostFilter = enumType({
   name: 'PostFilter',
@@ -94,9 +80,9 @@ const Query = queryType({
     t.field('posts', {
       type: list(Post),
       args: { filter: PostFilter },
-      resolve: (_parent, { filter }) => {
+      resolve: (_parent, { filter }, context) => {
         if (filter === 'Home') {
-          return TEST_POSTS
+          return context.prisma.post.findMany({ take: 10 })
         }
         if (filter === 'Mine') {
           return db.query()
